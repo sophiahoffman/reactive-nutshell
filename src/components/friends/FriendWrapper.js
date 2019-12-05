f81818aac869abf1ee23268622a0bc47bbcdae26
Ken Boyd <klb417@gmail.com>
2019-12-05 09:29:00 -0600

    added async await examples in API manager

--- src/components/friends/FriendWrapper.js
+++ src/components/friends/FriendWrapper.js
@@ -9,48 +9,67 @@ class FriendWrapper extends Component {
   componentDidMount() {
     this.getFriends();
   }
-  getFriends = () => {
-    return APIManager.get(
+  getFriends = async () => {
+    // return APIManager.get(
+    //   `friends/?loggedInUser=${localStorage.getItem("userId")}&_expand=user`
+    // ).then(friends => {
+    //   this.setState({ friends: friends });
+    // });
+    const friends = await APIManager.get(
       `friends/?loggedInUser=${localStorage.getItem("userId")}&_expand=user`
-    ).then(friends => {
-      this.setState({ friends: friends });
-    });
+    )
+    this.setState({ friends: friends });
   };
-  addFriend = id => {
+  addFriend = async id => {
     if (Number(localStorage["userId"]) === Number(id)) {
       window.alert(
         "You cannot add yourself as a friend"
       );
     } else {
-      return APIManager.get(`friends?loggedInUser=${localStorage["userId"]}`)
-        .then(friends => {
-          if (friends.find(friend => Number(friend.userId) === Number(id))) {
-            window.alert("That user is already a friend");
-          } else {
-            return APIManager.post(`friends/`, {
-              loggedInUser: Number(localStorage["userId"]),
-              userId: Number(id)
-            });
-          }
-        })
-        .then(() => {
-          this.getFriends();
+      // return APIManager.get(`friends?loggedInUser=${localStorage["userId"]}`)
+      //   .then(friends => {
+      //     if (friends.find(friend => Number(friend.userId) === Number(id))) {
+      //       window.alert("That user is already a friend");
+      //     } else {
+      //       return APIManager.post(`friends/`, {
+      //         loggedInUser: Number(localStorage["userId"]),
+      //         userId: Number(id)
+      //       });
+      //     }
+      //   })
+      //   .then(() => {
+      //     this.getFriends();
+      //   });
+      const friends = await APIManager.get(`friends?loggedInUser=${localStorage["userId"]}`)
+      if (friends.find(friend => Number(friend.userId) === Number(id))) {
+        window.alert("That user is already a friend");
+      } else {
+        await APIManager.post(`friends/`, {
+          loggedInUser: Number(localStorage["userId"]),
+          userId: Number(id)
         });
+      }
+      await this.getFriends();
     }
   };
-  removeFriend = id => {
+  removeFriend = async id => {
     if (Number(localStorage["userId"]) === Number(id)) {
       window.alert(
         "Not sure how you did that, but you cannot delete yourself as a friend"
       );
     } else {
-      return APIManager.get(
+      // return APIManager.get(
+      //   `friends?loggedInUser=${localStorage["userId"]}&userId=${id}`
+      // ).then(friendResponse => {
+      //   return APIManager.delete(`friends/${friendResponse[0].id}`).then(() => {
+      //     this.getFriends();
+      //   });
+      // });
+      const friendResponse = await APIManager.get(
         `friends?loggedInUser=${localStorage["userId"]}&userId=${id}`
-      ).then(friendResponse => {
-        return APIManager.delete(`friends/${friendResponse[0].id}`).then(() => {
-          this.getFriends();
-        });
-      });
+      );
+      await APIManager.delete(`friends/${friendResponse[0].id}`);
+      await this.getFriends();
     }
   };
 
